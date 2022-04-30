b688a882b988d81cb1bd892781e59a3b407b48fe
oumobayid <elobayidoumaima@gmail.com>
2022-04-30 20:32:16 +0000

    update

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    protected $model = Student::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+        ];
+    }
+}
