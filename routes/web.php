793393baee4f1ce5fc38fe2ed383113ba9ec7690
oumobayid <elobayidoumaima@gmail.com>
2022-05-02 23:57:28 +0000

    suite

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Front\CourseController;
 use App\Http\Controllers\Front\ContactController;
 use App\Http\Controllers\front\MessageController;
@@ -16,10 +17,18 @@ use App\Http\Controllers\Front\HomepageController;
 |
 */
 
-Route::get('/', [HomepageController::class, 'index'])->name('front.homepage');
-Route::get('/cat/{id}', [CourseController::class, 'cat'])->name('front.cat');
-Route::get('/course/{c_id}', [CourseController::class, 'show'])->name('front.show');
-Route::get('/contact', [ContactController::class, 'index'])->name('front.contact');
-Route::post('/message/newsletter', [MessageController::class, 'newsletter'])->name('front.message.newsletter');
-Route::post('/message/contact', [MessageController::class, 'contact'])->name('front.message.contact');
-Route::post('/message/enroll', [MessageController::class, 'enroll'])->name('front.message.enroll');
+Route::namespace('Front')->group(function(){
+    Route::get('/', [HomepageController::class, 'index'])->name('front.homepage');
+    Route::get('/cat/{id}', [CourseController::class, 'cat'])->name('front.cat');
+    Route::get('/course/{c_id}', [CourseController::class, 'show'])->name('front.show');
+    Route::get('/contact', [ContactController::class, 'index'])->name('front.contact');
+    Route::post('/message/newsletter', [MessageController::class, 'newsletter'])->name('front.message.newsletter');
+    Route::post('/message/contact', [MessageController::class, 'contact'])->name('front.message.contact');
+    Route::post('/message/enroll', [MessageController::class, 'enroll'])->name('front.message.enroll');
+    
+});
+
+Route::namespace('Front')->group(function(){
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.home');
+   
+});
