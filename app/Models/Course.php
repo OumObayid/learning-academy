4a0c03f5138322508215464ece89d5176e1d5155
Oumaima El Obayid <86168482+OumObayid@users.noreply.github.com>
2022-05-20 16:22:00 +0100

    Comment

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,10 +9,14 @@ class Course extends Model
 {
       //use HasFactory;
       protected $guarded = ['id'];
+
+   //Creation de la relation many-1: Course-Cat
       public function cat()
       {
           return $this->belongsTo('App\Models\Cat');
       }
+
+//Creation de la relation many-1: Course-Trainer
       public function trainer()
       {
           return $this->belongsTo('App\Models\Trainer');
