1fa7d16edcaae5e6114903f60df3ce2813fc9e0f
Oumaima El Obayid <86168482+OumObayid@users.noreply.github.com>
2022-05-21 20:42:18 +0200

    update

--- resources/views/admin/setting/edit.blade.php
+++ resources/views/admin/setting/edit.blade.php
@@ -37,17 +37,12 @@
                         class="my-3" /><input name="favicon" class="form-control-file" type="file">
                 </td>
             </tr>
-            <tr>
-                <td> <tr>
+            <tr>               
                 <td>Background</td>
                 <td><img src="{{ asset('uploads/setting/' . $setting->background) }}" width="250"
                         class="my-3" /><input name="favicon" class="form-control-file" type="file">
                 </td>
-            </tr></td>
-                <td><img src="{{ asset('uploads/setting/' . $setting->favicon) }}" width="50"
-                        class="my-3" /><input name="favicon" class="form-control-file" type="file">
-                </td>
-            </tr>
+            </tr>           
             <tr>
                 <td>City</td>
                 <td><input name="city" class="form-control" type="text" value="{{ $setting->city }}"></td>
